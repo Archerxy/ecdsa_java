adba8cd5bd4b02e8189fee549d94bdce8cd43324
Archerxy <tagaryen199@163.com>
2021-01-28 10:41:58 +0800

    format code

--- archer/test/EcdsaTest.java
+++ archer/test/EcdsaTest.java
@@ -10,21 +10,22 @@ public class EcdsaTest {
     public static void secpTest() {
     	String priKeyHex = "43EFA45ABDA29F5E4A7FEFABC3DACF7ACFF4EAAF861AF5DFFF3AD1F6543F3ACA";
     	String hashStr = "FAE432CE3DDAFCE3457FEFABC3DACF7ACFF4EAAF861AF5DFFF3AFACEB56D4AA4";
+
     	byte[] privBytes = NumberUtil.hexStrToBytes(priKeyHex);
     	byte[] hashBytes = NumberUtil.hexStrToBytes(hashStr);
-    	
+
     	Ecdsa secp256k1 = Ecdsa.from(Curve.SECP_256_K1);
-    	
+
     	String sig = secp256k1.sign(privBytes, hashBytes);
-		System.out.println(sig);
+    	System.out.println(sig);
     	
-		byte[] pubBytes = secp256k1.privateKeyToPublicKey(privBytes);
-		System.out.println(NumberUtil.bytesToHexStr(pubBytes));
+
+    	byte[] pubBytes = secp256k1.privateKeyToPublicKey(privBytes);
+    	System.out.println(NumberUtil.bytesToHexStr(pubBytes));
 		
-		System.out.println(secp256k1.verify(pubBytes, hashBytes, sig));
+    	System.out.println(secp256k1.verify(pubBytes, hashBytes, sig));
 		
-		byte[] pubBs = secp256k1.recoverToPublicKey(hashBytes, sig);
-		System.out.println(NumberUtil.bytesToHexStr(pubBs));
-    	
+    	byte[] pubBs = secp256k1.recoverToPublicKey(hashBytes, sig);
+    	System.out.println(NumberUtil.bytesToHexStr(pubBs));
     }
 }
